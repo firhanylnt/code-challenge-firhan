d3e31d5cb7e55bb68899054798f8b5fd5bb01b9c
firhan14 <firhan@visinemapictures.com>
2024-10-15 19:36:36 +0700

    update alt image

--- src/views/pages/home/company-overview.tsx
+++ src/views/pages/home/company-overview.tsx
@@ -37,12 +37,12 @@ export default async function CompanyOverview() {
 
             <h1 className="text-2xl md:text-4xl font-bold text-center p-4 m-[1em]">Kenalin, Gojek. Si Pembawa Perubahan</h1>
 
-            <div className="bg-white shadow-md rounded-tr-[8%] rounded-tl-[8%] p-6 mb-8 w-full md:mt-[10em]">
+            <div className="bg-white shadow-md rounded-tr-[8%] rounded-tl-[8%] rounded-br-[8%] rounded-bl-[8%] p-8 mb-8 w-full md:mt-[10em]">
                 <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 text-center">
 
                 {overviews && overviews.items?.map((overview, idx) => (
                     <div className="p-4 text-center md:mt-[-6em]" key={idx}>
-                        <img className="mx-auto" width={200} height={200} src={`https:${(overview.fields.media as IAsset).fields.file.url}`} />
+                        <img className="mx-auto" alt={overview.fields.title} width={200} height={200} src={`https:${(overview.fields.media as IAsset).fields.file.url}`} />
                         <h3 className="text-black mt-4 text-xl font-semibold">{overview.fields.title}</h3>
                         <div className='text-black'>
                             <RichText document={overview.fields.content} />
@@ -56,8 +56,8 @@ export default async function CompanyOverview() {
 
                 <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
                     {services && services.items?.map((service, idx) => (
-                        <div className={`bg-[${service.fields.color}] py-[2em] md:py-0 px-[3em] rounded-[10%] shadow-md md:h-[25vw] shadow-red md:mt-[5em]`} key={idx}>
-                            <img className="mx-auto md:mt-[-5em]" width={400} height={400} src={`https:${(service.fields.media as IAsset).fields.file.url}`} />
+                        <div className={`bg-[#65d75d] py-[2em] md:py-0 px-[3em] rounded-[10%] shadow- md:h-[25vw] md:mt-[5em] md:mb-[5em]`} key={idx}>
+                            <img className="mx-auto md:mt-[-5em]" alt={`gojek-`+service.fields.title} width={400} height={400} src={`https:${(service.fields.media as IAsset).fields.file.url}`} />
                             <h3 className="text-black mt-4 text-[40px] font-semibold md:mt-[2em]">{service.fields.title}</h3>
                             <div className='text-black'>
                                 <RichText document={service.fields.content} />
