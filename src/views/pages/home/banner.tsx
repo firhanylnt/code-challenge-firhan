ee9f67b9e2bc4241e46228dd048c1585bc66cab3
firhan14 <firhan@visinemapictures.com>
2024-10-18 13:46:14 +0700

    update font size

--- src/views/pages/home/banner.tsx
+++ src/views/pages/home/banner.tsx
@@ -16,10 +16,10 @@ export default function BannerHome() {
       <div className="relative z-10 flex items-center justify-center h-full bg-black bg-opacity-50">
         <div className="text-center text-white p-4 md:text-center">
           {/* <h1 className="text-4xl font-bold md:text-6xl">GOJEK</h1> */}
-          <p className="text-4xl md:text-5xl font-bold">
+          <p className="text-2xl md:text-5xl font-bold">
           3 negara. 20+ layanan.
           </p>
-          <p className="text-4xl md:text-5xl font-bold">
+          <p className="text-2xl md:text-5xl font-bold">
           1 platform on-demand terkemuka.
           </p>
         </div>
